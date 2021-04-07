7a415ae8d5eee232d99f6056a51edfbadf4bba65
Glitch (cityhive) <none>
2021-04-07 20:03:43 +0000

    🐁⛄️ Checkpoint ./server.js:911846/6296

--- server.js
+++ server.js
@@ -6,12 +6,7 @@
 const express = require("express");
 const app = express();
 
-const fetch = require('node-fetch');
-module.exports = (url, args = {}) => {
-  args.headers = args.headers || {}
-  args.headers['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
-  return fetch(url, args)
-}
+const fetch = require("node-fetch");
 
 // our default array of dreams
 const dreams = [
@@ -40,13 +35,30 @@ const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
-
 let options = {
-    // These properties are part of the Fetch Standard
-    method: 'GET',
-    headers: {},        // request headers. format is the identical to that accepted by the Headers constructor (see below)
+  method: "GET",
+  headers: {
+    accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "no-cache",
+    cookie:
+      "csrftoken=Vp0SJZiWJaqlBQCajZoWhqxp7fBscfK9; mid=YG4PMwALAAGMQI8ip1lawjvyeKyL; ig_did=75872AC2-855B-494B-8D92-B1E49BDF82B1; ig_nrcb=1",
+    pragma: "no-cache",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36 Edg/89.0.774.68"
+  }
 };
 
-fetch('https://httpbin.org/post', { method: 'POST', body: 'a=1' })
-    .then(res => res.text()) // expecting a json response
-    .then(json => console.log(json));
+fetch(
+  "https://www.instagram.com/graphql/query/?query_id=17888483320059182&id=9270148917&first=50",
+  options
+)
+  .then(res => res.text()) // expecting a json response
+  .then(json => console.log(json));
