0cde90d00058f0b089724719cc422f5fca1a7209
Glitch (cityhive) <none>
2021-04-07 22:10:26 +0000

    🍏🌕 Checkpoint ./public/script.js:911846/3861 ./views/index.html:911846/110

--- public/script.js
+++ public/script.js
@@ -1,32 +1,43 @@
 const App = {
   data() {
     return {
-      instagramRawData: [],
+      instagramRawData: []
     };
   },
-  compuated: {
+  computed: {
     instagramData() {
-      instagramRawData.map(post => {
+      return this.instagramRawData.map(post => {
+        const caption = post.node.edge_media_to_caption.edges[0].node.text;
+        
         return {
-          
+          img: post.node.display_url,
+          thumbnail: post.node.thumbnail_src,
+          caption: caption,
+          hashTags: caption.match(/#\w+/g),
         };
       });
-      return [];
     },
     hashtags() {
-      let array = [];
+      let tags = {};
       this.instagramData.forEach(post => {
-        text = 
-        .match(/#\w+/g)
+        if (post.hashTags !== null) {
+        post.hashTags.forEach(hashTag => {
+          if (tags[hashTag] === undefined) {
+            tags[hashTag] = 1;
+          }
+          tags[hashTag]++;
+        });
       });
-      return array;
+      return tags;  
+        }
+        
     }
   },
   created() {
     fetch("/instagram-data")
       .then(response => response.json()) // parse the JSON from the server
       .then(data => {
-        this.instagramData.push(...data);
+        this.instagramRawData.push(...data);
       });
   }
 };
